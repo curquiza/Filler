ec87b9c5ec52f8b77bd8a38a71a356b180e11533
Clementine URQUIZAR <curquiza@e2r9p20.42.fr>
2017-09-27 18:34:39 +0200

    Add : filler.h -> struct s_map et s_game

---
+++ includes/filler.h
@@ -0,0 +1,29 @@
+#ifndef FILLER_H
+# define FILLER_H
+
+# include "libft.h"
+
+typedef struct	s_map
+{
+	char	value;
+	int		x;
+	int		y;
+	int		weight;
+	int		heat;
+	int		border;
+}				t_map
+
+typedef struct	s_game
+{
+	char	my_coin;
+	char	opp_coin;
+	int		h_map;
+	int		w_map;
+	char	*gross_map;
+	t_map	**strat_map;
+	char	*piece;
+	int		h_piece;
+	int		w_piece;
+}				t_game
+
+#endif 
