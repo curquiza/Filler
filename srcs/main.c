3d8d80022757b0d166fa7517574cfccdbd899c3f
Clurquiz <clementine.urquizar@gmail.com>
2017-09-29 12:36:57 +0200

    Upd : function debug modified

--- srcs/main.c
+++ srcs/main.c
@@ -143,7 +143,7 @@ void	ft_debug(t_game game)
 {
 	int		fd;
 
-	fd = open("debug", O_RDWR | O_CREAT, 0666);
+	fd = open("debug", O_RDWR | O_CREAT | O_TRUNC, 0666);
 	ft_putstr_fd("my_coin : ", fd);
 	ft_putchar_fd(game.my_coin, fd);
 	ft_putendl_fd("", fd);
@@ -176,30 +176,6 @@ int		main(void)
 	if (ft_get_first_data(&game) == -1)
 		ft_exit("Read error", 2);
 	ft_debug(game);
-	while (1);
+	//while (1);
 	return (0);
 }
-
-/*int		main(void)
-{
-	char	line[10001];
-	int		fd;
-
-	//get_next_line(0, &line);
-	ft_bzero(line, 10001);
-	read(0, line, 10000);
-	fd = open("debug", O_RDWR | O_CREAT | O_APPEND, 0666);
-	ft_putendl_fd("--- NOUVELLE LECTURE ---", fd);
-	ft_putendl_fd(line, fd);
-	ft_putendl("8 2");
-	//sleep(2);
-	ft_putendl_fd("--- NOUVELLE LECTURE ---", fd);
-	ft_bzero(line, 10001);
-	//ft_putnbr_fd(read(0, line, 10000), fd);
-	read(0, line, 10000);
-	ft_putendl_fd(line, fd);
-	//sleep(2);
-	//ft_putendl("8 2");
-	//close (fd);
-	return (0);
-}*/
