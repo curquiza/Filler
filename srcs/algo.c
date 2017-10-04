c87555f2169234f1dd48d30c35e7dd4d2733d2df
Clementine URQUIZAR <curquiza@e2r8p13.42.fr>
2017-10-04 18:38:30 +0200

    Add : ft_go_down for heat_map

--- srcs/algo.c
+++ srcs/algo.c
@@ -1,5 +1,19 @@
 #include "filler.h"
 
+int		ft_is_opp(t_game *game, char value)
+{
+	if (value == game->opp_coin || value == game->opp_coin - 32)
+		return (1);
+	return (0);
+}
+
+int		ft_is_empty(char value)
+{
+	if (value == '.')
+		return (1);
+	return (0);
+}
+
 static void	ft_init_heat_map(t_game *game, t_heat_map ***heat_map)
 {
 	int		i;
@@ -13,11 +27,39 @@ static void	ft_init_heat_map(t_game *game, t_heat_map ***heat_map)
 	}
 }
 
+void	ft_go_down(t_game *game, t_heat_map **map)
+{
+	int		i;
+	int		j;
+	int		w;
+
+	w = game->h_map;
+	j = 0;
+	while (j < game->w_map)
+	{
+		i = 0;
+		w = game->h_map;
+		while (i < game->h_map)
+		{
+			if (i != 0
+				&& ft_is_empty(game->strat_map[i][j].value)
+				&& ft_is_opp(game, game->strat_map[i - 1][j].value))
+				map[i][j].down = w;
+			w--;
+			if (ft_is_opp(game, game->strat_map[i][j].value))
+				w = game->h_map;
+			i++;
+		}
+		j++;
+	}
+}
+
 void	ft_calculate_heat_weight(t_game *game)
 {
 	t_heat_map **heat;
 
 	ft_init_heat_map(game, &heat);
+	ft_go_down(game, heat);
 }
 
 int		ft_put_piece(t_game *game)
