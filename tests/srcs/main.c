7ea9dbf82ab3e43fb972ff2e8b1a249461380491
Clurquiz <clementine.urquizar@gmail.com>
2018-01-01 14:57:20 +0100

    Add : structure of tests

---
+++ tests/srcs/main.c
@@ -0,0 +1,58 @@
+#include "filler.h"
+
+static	int	ft_read_input(t_game *game, char *av)
+{
+	char	*line;
+	char	*tmp;
+	int		cpt;
+	int		len;
+	int		fd;
+
+	if ((fd = open(av, O_RDONLY)) <= 0)
+		return (-1);
+	cpt = 1;
+	len = 0;
+	while (get_next_line(fd, &line) > 0)
+	{
+		if (cpt == 1 && line)
+			len = ft_strlen(line);
+		else if (line && len != (int)ft_strlen(line))
+		{
+			ft_putendl_fd("Width error", 2);
+			return (-1);
+		}
+		tmp = game->gross_map;
+		game->gross_map = ft_strjoin3(tmp, line, "\n");
+		ft_strdel(&tmp);
+		ft_strdel(&line);
+		cpt++;
+	}
+	game->h_map = cpt;
+	game->w_map = len;
+	return (0);
+}
+
+static void	ft_get_coin(t_game *game, char *coin)
+{
+	if (!coin || (coin && coin[0] == 'x'))
+	{
+		game->my_coin = 'x';
+		game->opp_coin = 'o';
+	}
+	else
+	{
+		game->my_coin = 'o';
+		game->opp_coin = 'x';
+	}
+}
+
+int		main(int ac, char **av)
+{
+	t_game game;
+	
+	if (ac <= 1)
+		ft_exit("No input", 2);
+	if (ft_read_input(&game, av[1]) == -1)
+		ft_exit("Read error", 2);
+	ft_get_coin(&game, av[2]);
+}
