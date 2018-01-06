374c3c0326a201ede6ad9f0fba95d6152a8fee4c
Clementine URQUIZAR <curquiza@e2r9p14.42.fr>
2018-01-06 19:09:22 +0100

    Upd : placement and score done

--- srcs/algo.c
+++ srcs/algo.c
@@ -7,17 +7,65 @@ void	ft_calc_heat_weight(t_game *game)
 	ft_fill_heat_2(game);
 }
 
+void	ft_deplacement(char piece_value, int *i, int *j, int init_j)
+{
+	if (piece_value == '\n')
+	{
+		*j = init_j;
+		(*i)++;
+	}
+	else
+		(*j)++;
+}
+
+int 	ft_calc_score(t_game game, int i, int j)
+{
+	float	score;
+	int		my_coin;
+	int		init_j;
+
+	score = 0;
+	my_coin = 0;
+	init_j = j;
+	while (*game.piece)
+	{
+		if (game.piece == '*')
+		{
+			if (j >= game.w || i >= game.w)
+				return (0);
+			if (ft_is_me(game, game.strat_map[i][j].value) && my_coin == 0)
+				my_coin = 1
+			else if (ft_is_me(game, game.strat_map[i][j].value) && my_coin == 1)
+				return (0);
+			else if (ft_is_opp(game, game.strat_map[i][j].value))
+				return (0);
+			else if (ft_is_empty(game, game.strat_map[i][j].value))
+				score += game.strat_map[i][j].weight;
+		}
+		game.piece++;
+		ft_deplacement(*game.piece, i, j, init_j);
+	}
+	return (score);
+}
+
 void	ft_get_place(t_game *game)
 {
 	int		i;
 	int		j;
+	float	best_score;
 
+	best_score = 0;
 	i = 0;
 	while (i < game->h_map)
 	{
 		j = 0;
 		while (j < game->w_map)
 		{
+			if (ft_calc_score(*game, i, j) > best_score)
+			{
+				game->pos_x = i;
+				game->pos_y = j;
+			}
 			j++;
 		}
 		i++;
