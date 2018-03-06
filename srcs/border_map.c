0e7e48b580e97469b8942c0d9547cbaa1f9e335e
Clementine <clementine@MacBook-Pro-de-Clementine.local>
2018-03-06 13:54:46 +0100

    Add : fill from the left

--- srcs/border_map.c
+++ srcs/border_map.c
@@ -37,8 +37,33 @@ void	ft_fill_from_top(t_game *game)
 	}
 }
 
+void	ft_fill_from_left(t_game *game)
+{
+	int		i;
+	int		j;
+	int		value;
+	int		max_border;
+
+	max_border = ft_max(game->h_map, game->w_map);
+	j = 0;
+	value = max_border;
+	while (j < game->w_map)
+	{
+		i = 0;
+		while (i < game->h_map)
+		{
+			game->strat_map[i][j].border += value;
+			i++;
+		}
+		j++;
+		value--;
+	}
+}
+
 void	ft_calc_border_weight(t_game *game)
 {
 	if (game->border.top == 1)
 		ft_fill_from_top(game);
+	if (game->border.left == 1)
+		ft_fill_from_left(game);
 }
