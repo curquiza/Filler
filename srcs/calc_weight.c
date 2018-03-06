738b5e0fda32db8f70ac8a0d48857fb7b56e6dea
Clementine <clementine@MacBook-Pro-de-Clementine.local>
2018-03-06 13:39:42 +0100

    Upd : borders take in account in weight cal

--- srcs/calc_weight.c
+++ srcs/calc_weight.c
@@ -3,11 +3,12 @@
 void		ft_calc_weight(t_map *point, int h_map, int w_map)
 {
 	int		max_heat;
+	int		max_border;
 
 	max_heat = ft_max(h_map, w_map) * 4;
-	// max_border = ??
+	max_border = ft_max(h_map, w_map) * 2;
 	point->weight = (COEF_HEAT * point->heat / max_heat
-					+ COEF_BORDER * point->border)
-					// + COEF_BORDER * point->border / max_border)
+					// + COEF_BORDER * point->border
+					+ COEF_BORDER * point->border / max_border)
 					* 100;
 }
